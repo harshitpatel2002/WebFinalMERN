9ac37e6cb110d5518bd4cd418a8b9b6a6e4f102c
Harshit Patel <harshit.patel@edu.sait.ca>
2023-02-01 19:05:15 -0700

    Second Commit

--- server/index.js
+++ server/index.js
@@ -33,6 +33,18 @@ app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 app.use("/home", homeRouter);
 
-app.listen(5000, () => {
-	console.log("Server started at port 5000");
+if (
+	process.env.NODE_ENV === "production" ||
+	process.env.NODE_ENV === "staging"
+) {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname + "/client/build/index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server started at port ${PORT}`);
 });
